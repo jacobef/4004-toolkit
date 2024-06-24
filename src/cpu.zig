641a156af6b69cb1c0e179aaad1a06b9c045aeb9
jacobef <jacobef2@gmail.com>
2024-06-24 13:08:19 -0600

    4004 emulator with a few instructions

---
+++ src/cpu.zig
@@ -0,0 +1,71 @@
+const std = @import("std");
+const time = std.time;
+const instructions = @import("instruction_spec.zig");
+
+pub const Intel4004 = struct {
+    index_registers: [16]u4,
+    program_counter: u12,
+    accumulator: u4,
+    carry_bit: u1,
+    test_signal: u1,
+    stack_registers: [3]u12,
+    stack_pointer: u2,
+    src_address: u8,
+    dram: DataRAM,
+    pram: ProgramRAM,
+
+    pub fn pushToStack(self: *Intel4004, address: u12) void {
+        self.stack_registers[self.stack_pointer] = address;
+        self.stack_pointer = if (self.stack_pointer < 2) self.stack_pointer + 1 else 0;
+    }
+
+    pub fn popFromStack(self: *Intel4004) u12 {
+        self.stack_pointer = if (self.stack_pointer > 0) self.stack_pointer - 1 else 2;
+        return self.stack_registers[self.stack_pointer];
+    }
+
+    pub fn reset(self: *Intel4004) void {
+        self.* = std.mem.zeroes(Intel4004);
+    }
+
+    pub const DataRAM = struct {
+        pub const Register = struct { data: [16]u4, status: [4]u4 };
+        banks: [8][4][4]Register,
+        ports: [4]u4,
+        selected_bank: u3,
+    };
+    pub const ProgramRAM = struct { bytes: [4096]u8, write_enable: bool, wpm_half_byte: u1 };
+    pub const ROM = struct {
+        pub const IOLine = struct { in_or_out: enum { in, out }, status: bool };
+        bytes: [4096]u8,
+        ports: [16][4]IOLine,
+    };
+
+    pub const ExecutionError = error{IllegalInstructionError};
+
+    const clock_speed_hz: u64 = 740_000;
+    const ns_per_cycle: u64 = 1_000_000_000 / clock_speed_hz;
+
+    pub fn single_step(self: *Intel4004) !void {
+        // Original single_step code
+        const byte1 = self.pram.bytes[self.program_counter];
+        const inst_spec = instructions.getInstructionSpec(byte1) orelse return ExecutionError.IllegalInstructionError;
+        const n_bytes = inst_spec.opcode_string.len / 8;
+        const byte2 = if (n_bytes == 2) self.pram.bytes[self.program_counter + 1] else null;
+        self.program_counter +%= @intCast(n_bytes);
+        const args = inst_spec.extractArgs(byte1, byte2);
+
+        switch (inst_spec.func) {
+            .f_no_args => |f| f(self),
+            .f_3 => |f| f(self, @intCast(args[0])),
+            .f_4 => |f| f(self, @intCast(args[0])),
+            .f_12 => |f| f(self, @intCast(args[0])),
+            .f_3_8 => |f| f(self, @intCast(args[0]), @intCast(args[1])),
+            .f_4_8 => |f| f(self, @intCast(args[0]), @intCast(args[1])),
+        }
+
+        // for (0..nops_per_cycle) |_| {
+        //     asm volatile ("" ::: "memory");
+        // }
+    }
+};
---
+++ src/instruction_spec.zig
@@ -0,0 +1,322 @@
+const std = @import("std");
+const ArrayList = std.ArrayList;
+const cpus = @import("cpu.zig");
+const Type = std.builtin.Type;
+const Intel4004 = cpus.Intel4004;
+
+const InstructionFunction = union(enum) {
+    f_no_args: *const fn (cpu: *Intel4004) void,
+    f_3: *const fn (cpu: *Intel4004, arg: u3) void,
+    f_4: *const fn (cpu: *Intel4004, arg: u4) void,
+    f_12: *const fn (cpu: *Intel4004, arg: u12) void,
+    f_3_8: *const fn (cpu: *Intel4004, arg1: u3, arg2: u8) void,
+    f_4_8: *const fn (cpu: *Intel4004, arg1: u4, arg2: u8) void,
+};
+
+fn ins(mnemonic: []const u8, opcode: []const u8, func: anytype) InstructionSpec {
+    comptime {
+        const arg_extractors = getArgExtractors(opcode);
+        const arg_count = arg_extractors.len;
+
+        const func_union: InstructionFunction = switch (arg_count) {
+            0 => .{ .f_no_args = func },
+            1 => blk: {
+                const arg_bits = @popCount(arg_extractors[0].mask);
+                break :blk switch (arg_bits) {
+                    3 => .{ .f_3 = func },
+                    4 => .{ .f_4 = func },
+                    12 => .{ .f_12 = func },
+                    else => @compileError("Unsupported argument bit count"),
+                };
+            },
+            2 => blk: {
+                const arg1_bits = @popCount(arg_extractors[0].mask);
+                const arg2_bits = @popCount(arg_extractors[1].mask);
+                break :blk switch (arg1_bits) {
+                    3 => if (arg2_bits == 8) .{ .f_3_8 = func } else @compileError("Second argument must be 8 bits for f_3_8"),
+                    4 => if (arg2_bits == 8) .{ .f_4_8 = func } else @compileError("Second argument must be 8 bits for f_4_8"),
+                    else => @compileError("First argument must be 3 or 4 bits for two-argument functions"),
+                };
+            },
+            else => @compileError("Unsupported number of arguments"),
+        };
+
+        return InstructionSpec{
+            .mnemonic = mnemonic,
+            .arg_extractors = arg_extractors,
+            .opcode_string = opcode,
+            .func = func_union,
+        };
+    }
+}
+
+pub const InstructionSpec = struct {
+    mnemonic: []const u8,
+    arg_extractors: []const ArgExtractor,
+    opcode_string: []const u8,
+    func: InstructionFunction,
+
+    pub fn extractArgs(self: *const InstructionSpec, byte1: u8, byte2: ?u8) []const u16 {
+        const opcode: u16 =
+            if (byte2) |b2|
+            (@as(u16, byte1) << 8) | @as(u16, b2)
+        else
+            @as(u16, byte1) << 8;
+
+        const extrs = self.arg_extractors;
+
+        switch (self.arg_extractors.len) {
+            0 => return &.{},
+            1 => {
+                return &.{(opcode & extrs[0].mask) >> extrs[0].shift_amount};
+            },
+            2 => return &.{ (opcode & extrs[0].mask) >> extrs[0].shift_amount, (opcode & extrs[1].mask) >> extrs[1].shift_amount },
+            else => std.debug.panic("Only up to 2 arguments are supported", .{}),
+        }
+    }
+};
+
+pub const instructions = struct {
+    fn execute_nop(cpu: *Intel4004) void {
+        _ = cpu;
+    }
+
+    fn execute_inc(cpu: *Intel4004, reg: u4) void {
+        cpu.index_registers[reg] +%= 1;
+    }
+
+    fn execute_add(cpu: *Intel4004, reg: u4) void {
+        const result_before_carry = @addWithOverflow(cpu.index_registers[reg], cpu.accumulator);
+        const result = @addWithOverflow(result_before_carry[0], cpu.carry_bit);
+        cpu.accumulator = result[0];
+        cpu.carry_bit = result_before_carry[1] | result[1];
+    }
+
+    fn execute_ld(cpu: *Intel4004, reg: u4) void {
+        cpu.accumulator = cpu.index_registers[reg];
+    }
+
+    fn execute_xch(cpu: *Intel4004, reg: u4) void {
+        const old_accumulator: u4 = cpu.accumulator;
+        cpu.accumulator = cpu.index_registers[reg];
+        cpu.index_registers[reg] = old_accumulator;
+    }
+
+    fn execute_ldm(cpu: *Intel4004, val: u4) void {
+        cpu.accumulator = val;
+    }
+
+    pub const all = [_]InstructionSpec{
+        ins("NOP", "00000000", execute_nop),
+        ins("INC", "0110rrrr", execute_inc),
+        ins("ADD", "1000rrrr", execute_add),
+        ins("LD", "1010rrrr", execute_ld),
+        ins("XCH", "1011rrrr", execute_xch),
+        ins("LDM", "1101nnnn", execute_ldm),
+        // ins("SUB", "1001rrrr", execute_sub),
+        // ins("IAC", "11110010", execute_iac),
+        // ins("DAC", "11111000", execute_dac),
+        // ins("CLB", "11110000", execute_clb),
+        // ins("CLC", "11110001", execute_clc),
+        // ins("TCC", "11110111", execute_tcc),
+        // ins("CMC", "11110011", execute_cmc),
+        // ins("BBL", "1100nnnn", execute_bbl),
+        // ins("JMS", "0101aaaaaaaaaaaa", execute_jms),
+        // ins("JUN", "0100aaaaaaaaaaaa", execute_jun),
+        // ins("JIN", "0011ppp1", execute_jin),
+        // ins("FIM", "0010ppp0nnnnnnnn", execute_fim),
+        // ins("FIN", "0011ppp0", execute_fin),
+        // ins("JCN", "0001ccccaaaaaaaa", execute_jcn),
+        // ins("DAA", "11111011", execute_daa),
+        // ins("CMA", "11110100", execute_cma),
+        // ins("RAL", "11110101", execute_ral),
+        // ins("RAR", "11110110", execute_rar),
+        // ins("KBP", "11111100", execute_kbp),
+        // ins("STC", "11111010", execute_stc),
+        // ins("TCS", "11111001", execute_tcs),
+        // ins("ISZ", "0111rrrraaaaaaaa", execute_isz),
+        // ins("DCL", "11111101", execute_dcl),
+        // ins("SRC", "0010ppp1", execute_src),
+        // ins("RDM", "11101001", execute_rdm),
+        // ins("RD0", "11101100", execute_rd0),
+        // ins("RD1", "11101101", execute_rd1),
+        // ins("RD2", "11101110", execute_rd2),
+        // ins("RD3", "11101111", execute_rd3),
+        // ins("WR0", "11100100", execute_wr0),
+        // ins("WR1", "11100101", execute_wr1),
+        // ins("WR2", "11100110", execute_wr2),
+        // ins("WR3", "11100111", execute_wr3),
+        // ins("RDR", "11101010", execute_rdr),
+        // ins("ADM", "11101011", execute_adm),
+        // ins("WRM", "11100000", execute_wrm),
+        // ins("WRR", "11100010", execute_wrr),
+        // ins("WMP", "11100001", execute_wmp),
+        // ins("SBM", "11101000", execute_sbm),
+        // ins("WPM", "11100011", execute_wpm),
+    };
+};
+
+const OpcodeParseError = error{ invalid_opcode_char, invalid_opcode_length };
+
+fn addItem(comptime T: type, comptime list: []const T, comptime item: T) []const T {
+    return list ++ @as([]const T, &.{item});
+}
+
+pub fn splitOpcode(comptime opcode: []const u8) OpcodeParseError![]const []const u8 {
+    comptime {
+        if (opcode.len != 8 and opcode.len != 16) return .invalid_opcode_length;
+
+        var opcode_split: []const []const u8 = &.{};
+
+        var i: u8 = 0;
+        while (i < opcode.len) {
+            const seq_first = i;
+            if (opcode[i] >= 'a' and opcode[i] != 'z') {
+                while (i < opcode.len and opcode[i] == opcode[seq_first]) {
+                    i += 1;
+                }
+                opcode_split = addItem([]const u8, opcode_split, opcode[seq_first..i]);
+            } else if (opcode[i] == '0' or opcode[i] == '1') {
+                while (i < opcode.len and (opcode[i] == '0' or opcode[i] == '1')) {
+                    i += 1;
+                }
+                opcode_split = addItem([]const u8, opcode_split, opcode[seq_first..i]);
+            } else {
+                return .invalid_opcode_char;
+            }
+        }
+
+        return opcode_split;
+    }
+}
+
+const CPUArgType = enum { register, register_pair, number, address, condition };
+
+const CPUArgSpec = struct {
+    arg_type: CPUArgType,
+    n_bits: u4,
+};
+
+const ParsedOpcodePart = union(enum) {
+    arg: CPUArgSpec,
+    bits: []const u1,
+
+    pub fn format(
+        self: ParsedOpcodePart,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        _ = options;
+
+        switch (self) {
+            .arg => |arg| {
+                try writer.print("Arg(type={any}, n_bits={})", .{ arg.arg_type, arg.n_bits });
+            },
+            .bits => |bits| {
+                try writer.print("Bits(", .{});
+                for (bits) |bit| {
+                    try writer.print("{d}", .{bit});
+                }
+                try writer.print(")", .{});
+            },
+        }
+    }
+};
+
+const MAX_OPCODE_LENGTH = 16;
+
+fn getArgLocations(comptime opcode: []const u8) []const [2]u8 {
+    comptime {
+        const opcode_split = splitOpcode(opcode) catch |err| switch (err) {
+            OpcodeParseError.invalid_opcode_char => @compileError("invalid character in opcode"),
+            OpcodeParseError.invalid_opcode_length => @compileError("opcode length must be 8 or 16"),
+        };
+        var out: []const [2]u8 = &.{};
+        var bit_index: u8 = 0;
+
+        for (opcode_split) |seq| {
+            if (seq[0] == '0' or seq[0] == '1') {
+                bit_index += @intCast(seq.len);
+            } else {
+                out = addItem([2]u8, out, [2]u8{ bit_index, @intCast(seq.len) });
+                bit_index += @intCast(seq.len);
+            }
+        }
+        return out;
+    }
+}
+
+const ArgExtractor = struct {
+    mask: u16,
+    shift_amount: u4,
+};
+
+pub fn getArgExtractors(comptime opcode: []const u8) []const ArgExtractor {
+    comptime {
+        const parsedOpcode = getArgLocations(opcode);
+        var extractors: []const ArgExtractor = &.{};
+
+        for (parsedOpcode) |arg| {
+            const bit_index = arg[0];
+            const bit_length = arg[1];
+            var mask: u16 = 0;
+
+            for (bit_index..(bit_index + bit_length)) |bit_pos| {
+                mask |= (1 << (15 - bit_pos));
+            }
+            const shift_amount: u16 = 15 - (bit_index + bit_length - 1);
+            extractors = addItem(ArgExtractor, extractors, .{ .mask = mask, .shift_amount = shift_amount });
+        }
+
+        return extractors;
+    }
+}
+
+// thanks to sonnet 3.5 for the first draft of this function
+pub fn initOpcodeLookup() [256]?InstructionSpec {
+    comptime {
+        @setEvalBranchQuota(20000);
+        var lookup = [_]?InstructionSpec{null} ** 256;
+
+        for (instructions.all) |inst| {
+            var mask: u8 = 0;
+            var value: u8 = 0;
+
+            for (inst.opcode_string[0..8]) |c| {
+                mask <<= 1;
+                value <<= 1;
+
+                switch (c) {
+                    '0' => {
+                        mask |= 1;
+                    },
+                    '1' => {
+                        mask |= 1;
+                        value |= 1;
+                    },
+                    else => {}, // For argument bits (e.g. 'r', 'a'), we don't set the mask bit
+                }
+            }
+
+            // Fill in all matching opcodes
+            for (0..256) |opcode| {
+                if ((opcode & mask) == value) {
+                    if (lookup[opcode] != null) {
+                        @compileError("overlapping opcodes");
+                    }
+                    lookup[opcode] = inst;
+                }
+            }
+        }
+
+        return lookup;
+    }
+}
+
+const opcode_lookup = initOpcodeLookup();
+
+pub fn getInstructionSpec(first_8_bits: u8) ?InstructionSpec {
+    return opcode_lookup[first_8_bits];
+}
